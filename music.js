a6157cb36b8432f1c375271899710b24c9245682
GabrielaGarcia <149729146+GabissGarcia@users.noreply.github.com>
2024-11-09 16:25:06 -0300

    Update music.js

--- music.js
+++ music.js
@@ -1,7 +1,8 @@
 let musicas = [
-    {titulo:'Money', artista:'Lisa', src:'money.mp3', img:'Lisa.jpg'},
+    {titulo:'Money', artista:'Lisa', src:'money.mp3', img:'lisa.jpg'},
     {titulo:'Bring Me To Life', artista:'Evanescence', src:'Evanescence.mp3', img:'Evanescence.jpg'},
     {titulo:'My Universe', artista:'Coldplay X BTS', src:'BTS.mp3', img:'bts.jpg'}
+    
 ];
 
 let musica = document.querySelector('audio');
@@ -16,25 +17,25 @@ renderizarMusica(indexMusica);
 
 // Eventos
 document.querySelector('.botao-play').addEventListener('click', tocarMusica);
+
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica);
+
 musica.addEventListener('timeupdate', atualizarBarra);
 
 document.querySelector('.anterior').addEventListener('click', () => {
     indexMusica--;
     if (indexMusica < 0) {
-        indexMusica = musicas.length - 1;
+        indexMusica = 2;
     }
     renderizarMusica(indexMusica);
-    tocarMusica();  // Garantir que toque ao mudar de música
 });
 
 document.querySelector('.proxima').addEventListener('click', () => {
     indexMusica++;
-    if (indexMusica >= musicas.length){
+    if (indexMusica > 2){
         indexMusica = 0;
     }
     renderizarMusica(indexMusica);
-    tocarMusica();  // Garantir que toque ao mudar de música
 });
 
 // Funções
@@ -45,14 +46,11 @@ function renderizarMusica(index){
         nomeArtista.textContent = musicas[index].artista;
         imagem.src = musicas[index].img;
         duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
-        console.log('Música carregada:', musicas[index].titulo);
     });
 }
 
 function tocarMusica(){
-    musica.play()
-        .then(() => console.log("Música tocando"))
-        .catch((error) => console.log("Erro ao tocar música:", error));
+    musica.play();
     document.querySelector('.botao-pause').style.display = 'block';
     document.querySelector('.botao-play').style.display = 'none';
 }
@@ -76,5 +74,6 @@ function segundosParaMinutos(segundos){
     if (campoSegundos < 10){
         campoSegundos = '0' + campoSegundos;
     }
-    return campoMinutos + ':' + campoSegundos;
+
+    return campoMinutos+':'+campoSegundos;
 }
